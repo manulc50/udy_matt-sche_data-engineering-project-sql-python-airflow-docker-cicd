68838d47f7d8f6a0df996a2553d619b4577356ec
Manuel Lorenzo <manulc50@hotmail.com>
2026-06-02 14:23:53 +0200

    Cambio para ejecutar todas las tareas del pipeline

--- airflow/dags/main.py
+++ airflow/dags/main.py
@@ -19,7 +19,9 @@ default_args = {
     #"retries": 1,
     #"retry_delay: timedelta(minutes=5)",
     "max_active_runs": 1,
-    "dagrun_timeout": timedelta(hours=1)
+    "dagrun_timeout": timedelta(hours=1),
+    "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
+    #"end_date": datetime(2030, 12, 12, tzinfo=local_tz)
 }
 
 with DAG(
@@ -28,7 +30,7 @@ with DAG(
     default_args=default_args,
     schedule="0 14 * * *",
     catchup=False,
-    start_date=datetime(2025, 1, 1, tzinfo=local_tz),
+    #start_date=datetime(2025, 1, 1, tzinfo=local_tz),
     #end_date=datetime(2030, 12, 12, tzinfo=local_tz)
 ) as dag:
     
